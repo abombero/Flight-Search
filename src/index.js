b8e1dfa996aa28639941898462a36869ed6f60c9
abombero <agustin.wvonb@gmail.com>
2023-02-03 20:38:05 -0300

    final upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// Redux
+import { Provider } from "react-redux";
+import store from '../src/store';
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+
